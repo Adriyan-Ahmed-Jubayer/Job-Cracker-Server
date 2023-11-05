1bd5e2eea6a921ffedd8b982529ffe5b720b281c
Adriyan-Ahmed-Jubayer <adriyan.khan.jubayer@gmail.com>
2023-11-05 23:47:36 +0600

    Necessary Technology added & MongoDB Connected to the Database

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('This is the server of Job Cracker Website')
+} )
+
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.y6otyph.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const jobsCollection = client.db('assignment11DB').collection('jobs');
+    
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log(`The Server Of Job Cracker Website is Running on [${port}] Port`);
+})
