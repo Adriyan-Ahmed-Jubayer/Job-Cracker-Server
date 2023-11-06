9d17149bd531a7a6401d34870cb3075c1b34e9f2
Adriyan-Ahmed-Jubayer <adriyan.khan.jubayer@gmail.com>
2023-11-06 10:26:29 +0600

    JWT Token Implemented and recieve token in the server side successfully

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app;
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.accessToken;
+  if (!token) {
+    return res.status(401).send("unauthorized access");
+  }
+  jwt.verify(token, process.env.ACCEESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
-app.get('/', (req, res) => {
-    res.send('This is the server of Job Cracker Website')
-} )
-
+app.get("/", (req, res) => {
+  res.send("This is the server of Job Cracker Website");
+});
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.y6otyph.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -19,38 +40,47 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const jobsCollection = client.db('assignment11DB').collection('jobs');
-    
-    app.get('/api/v1/jobs', async(req, res) => {
-        const result = await jobsCollection.find().toArray();
-        res.send(result);
-    })
-    
-    app.get('/api/v1/jobs/:category', async(req, res) => {
-      const jobCategory = req.params.category;
-      const query = {Category: jobCategory };
-      console.log(query);
-      console.log(jobCategory);
-      const result = await jobsCollection.find(query).toArray();
-      console.log(result);
-      res.send(result);
+    const jobsCollection = client.db("assignment11DB").collection("jobs");
 
-    })
+    app.get("/api/v1/jobs", async (req, res) => {
+      let query = {};
+      const AccessToken = req.cookies?.token;
+      console.log(AccessToken);
+      if (req.query.category) {
+        query = { Category: req.query.category };
+      }
+      const result = await jobsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCEESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie('accessToken', token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ status: true });
+    });
 
     await client.connect();
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
   }
 }
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-    console.log(`The Server Of Job Cracker Website is Running on [${port}] Port`);
-})
+  console.log(`The Server Of Job Cracker Website is Running on [${port}] Port`);
+});
